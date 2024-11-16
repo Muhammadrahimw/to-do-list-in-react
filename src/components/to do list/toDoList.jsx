da2dc1e325b3c541977f59b771ad5446cb34e1d7
Muhammadrahim <muqalla21@gmail.com>
2024-11-16 15:58:04 +0500

    half code

--- src/components/to do list/toDoList.jsx
+++ src/components/to do list/toDoList.jsx
@@ -2,14 +2,35 @@ import styles from "./toDoList.module.scss";
 import {AiOutlineDelete} from "react-icons/ai";
 import {FiEdit} from "react-icons/fi";
 import {useRef} from "react";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 let ToDoList = () => {
 	let bodyRef = useRef(null);
 	let addRef = useRef(null);
+	let searchRef = useRef(null);
 	let [localData, setLocalData] = useState(
 		localStorage.getItem("txts") ? JSON.parse(localStorage.getItem("txts")) : []
 	);
+
+	let searchFunc = () => {
+		if (searchRef.current) {
+			let str = searchRef.current.value;
+			let newLocalData = localData.filter((value) =>
+				value.includes(String(str))
+			);
+			console.log(newLocalData);
+			if (newLocalData.length > 0) {
+				setLocalData(newLocalData);
+			} else {
+				setLocalData(JSON.parse(localStorage.getItem("txts")));
+			}
+		}
+	};
+
+	useEffect(() => {
+		searchFunc();
+	}, [searchRef.current ? searchRef.current.value : ""]);
+
 	let addLocalFunc = () => {
 		let addCardInput = addRef.current.value;
 		let newLocalData = [...localData, addCardInput];
@@ -19,7 +40,7 @@ let ToDoList = () => {
 	};
 	let deleteLocalFunc = (idx) => {
 		let childBody = Array.from(bodyRef.current.children);
-		let newElement = childBody.forEach((value) => {
+		childBody.forEach((value) => {
 			if (value.id == idx) {
 				let deleteData = JSON.parse(localStorage.getItem("txts"));
 				deleteData.splice(value.id, 1);
@@ -33,7 +54,13 @@ let ToDoList = () => {
 		<section className={styles.mainSection}>
 			<div className={styles.header}>
 				<h1>To Do List</h1>
-				<input id="searchCard" placeholder="Search" type="text" />
+				<input
+					ref={searchRef}
+					id="searchCard"
+					placeholder="Search"
+					type="text"
+					onChange={searchFunc}
+				/>
 				<div className={styles.addingCont}>
 					<input ref={addRef} id="addCard" placeholder="Add card" type="text" />
 					<button onClick={addLocalFunc} className={styles.btn}>

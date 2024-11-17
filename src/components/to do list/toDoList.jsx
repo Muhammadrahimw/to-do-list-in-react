5d6e0225c81379d40dc1ecafb6ed29cb5a9b4ddf
Muhammadrahim <muqalla21@gmail.com>
2024-11-17 18:12:54 +0500

    adding search

--- src/components/to do list/toDoList.jsx
+++ src/components/to do list/toDoList.jsx
@@ -1,38 +1,72 @@
-import styles from "./toDoList.module.scss";
 import {AiOutlineDelete} from "react-icons/ai";
 import {FiEdit} from "react-icons/fi";
+// icons
+import styles from "./toDoList.module.scss";
+import axios from "axios";
 import {useRef} from "react";
 import {useState, useEffect} from "react";
-import axios from "axios";
 
 let ToDoList = () => {
 	let bodyRef = useRef(null);
 	let addRef = useRef(null);
 	let searchRef = useRef(null);
-	let [localData, setLocalData] = useState(
-		localStorage.getItem("txts") ? JSON.parse(localStorage.getItem("txts")) : []
-	);
+	let [txts, setTxts] = useState([]);
+	let [inputValue, setInputValue] = useState("");
+	let baseData = useRef(null);
 
-	let searchFunc = () => {};
+	let getTxt = () => {
+		axios
+			.get(`http://localhost:3000/texts`)
+			.then((data) => {
+				setTxts(data.data);
+				baseData.current = data.data;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-	let addLocalFunc = () => {
-		let addCardInput = addRef.current.value;
-		let newLocalData = [...localData, addCardInput];
-		setLocalData(newLocalData);
-		localStorage.setItem("txts", JSON.stringify(newLocalData));
-		addRef.current.value = "";
+	useEffect(() => {
+		getTxt();
+	}, []);
+
+	let postTxt = () => {
+		if (addRef.current.value) {
+			let txt = addRef.current.value;
+			axios
+				.post(`http://localhost:3000/texts`, {
+					title: "New Post",
+					body: txt,
+				})
+				.then((data) => {
+					console.log("yuborildi: " + data.data);
+					getTxt();
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+			addRef.current.value = "";
+		}
 	};
-	let deleteLocalFunc = (idx) => {
-		let childBody = Array.from(bodyRef.current.children);
-		childBody.forEach((value) => {
-			if (value.id == idx) {
-				let deleteData = JSON.parse(localStorage.getItem("txts"));
-				deleteData.splice(value.id, 1);
-				setLocalData(deleteData);
-				localStorage.setItem("txts", JSON.stringify(deleteData));
-			}
-		});
+
+	let deleteFunc = (idx) => {
+		axios
+			.delete(`http://localhost:3000/texts/${idx}`)
+			.then((data) => {
+				console.log(`o'chirildi: ` + data.data);
+				getTxt();
+			})
+			.catch(`o'chirilmadi :( `);
 	};
+
+	useEffect(() => {
+		if (baseData.current) {
+			let filteredData = baseData.current.filter((item) =>
+				item.body.includes(inputValue)
+			);
+			setTxts(filteredData);
+		}
+	}, [inputValue]);
 
 	return (
 		<section className={styles.mainSection}>
@@ -43,29 +77,31 @@ let ToDoList = () => {
 					id="searchInput"
 					placeholder="Search"
 					type="text"
-					onChange={searchFunc}
+					onChange={(e) => setInputValue(e.target.value)}
 				/>
 				<div className={styles.addingCont}>
 					<input ref={addRef} id="addCard" placeholder="Add card" type="text" />
-					<button onClick={addLocalFunc} className={styles.btn}>
+					<button onClick={postTxt} className={styles.btn}>
 						Add
 					</button>
 				</div>
 			</div>
 			<div id="body" ref={bodyRef} className={styles.body}>
-				{localData.map((item, id) => (
-					<div id={id} key={id} className={styles.card}>
-						<input type="checkbox" />
-						<div>
-							<p>{item}</p>
+				{txts.map((item) => {
+					return (
+						<div id={item.id} key={item.id} className={styles.card}>
+							<input type="checkbox" />
+							<div>
+								<p>{item.body}</p>
+							</div>
+							<AiOutlineDelete
+								onClick={() => deleteFunc(item.id)}
+								id={item.id}
+								className={styles.deleteCard}
+							/>
 						</div>
-						<AiOutlineDelete
-							onClick={() => deleteLocalFunc(id)}
-							id={id}
-							className={styles.deleteCard}
-						/>
-					</div>
-				))}
+					);
+				})}
 			</div>
 		</section>
 	);
